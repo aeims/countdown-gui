671a2d9c9f4837f6ea332b2f4d489bb636b66895
Weisaw <meysamm74@gmail.com>
2022-07-06 19:57:55 +0430

    generalized

--- countdown.py
+++ countdown.py
@@ -4,10 +4,10 @@ from time import sleep
 our_date = datetime(year=2022, month=7, day=6, hour=14, minute=56)
 def cnt():
     countdown = our_date - datetime.now().replace(microsecond=0)
-    print(f"\rCountdown to our date💓: {countdown}", end='')
+    print(f"\rCountdown to date: {countdown}", end='')
 
     if countdown == timedelta(0):
-        print('\n🥳🍾yeeeaaah time to fuck!🎉😎')
+        print('\n🥳🍾yeeeaaah time to dance!🎉😎')
     else:
         sleep(1)
         cnt()

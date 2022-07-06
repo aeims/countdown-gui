7eb76dfaa205ac1744abde3aea8a9e680cdce28e
Weisaw <meysamm74@gmail.com>
2022-07-06 19:53:15 +0430

    first commit

---
+++ countdown.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from time import sleep
+
+our_date = datetime(year=2022, month=7, day=6, hour=14, minute=56)
+def cnt():
+    countdown = our_date - datetime.now().replace(microsecond=0)
+    print(f"\rCountdown to our date💓: {countdown}", end='')
+
+    if countdown == timedelta(0):
+        print('\n🥳🍾yeeeaaah time to fuck!🎉😎')
+    else:
+        sleep(1)
+        cnt()
+
+cnt()
---
+++ countdown_v2.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from tkinter import messagebox
+from datetime import datetime, timedelta
+# import sys
+# sys.setrecursionlimit(10000)
+
+def countdown(client_date, msg, name):
+    
+    cntdown = client_date - datetime.now().replace(microsecond=0)
+    lbl = Label(root, text=f"{name} Countdown: {cntdown}")
+    lbl.grid(row=8, column=0)
+
+    if cntdown == timedelta(0):
+        messagebox.showinfo('countdown', msg)
+        root.after(5000, root.destroy)
+    else:
+        root.after(1000, lambda: countdown(client_date, msg, name))
+
+
+root = Tk()
+root.title('countdown')
+
+
+lbl_name = Label(root, text='enter the name for countdown: ')
+entry_name = Entry(root)
+
+lbl_year = Label(root, text='enter the year: ')
+entry_year = Entry(root)
+
+lbl_month = Label(root, text='enter the month: ')
+entry_month = Entry(root)
+
+lbl_day = Label(root, text='enter the day:  ')
+entry_day = Entry(root)
+
+lbl_hour = Label(root, text='enter the hour: ')
+entry_hour = Entry(root)
+
+lbl_min = Label(root, text='enter the minute: ')
+entry_min = Entry(root)
+
+lbl_msg = Label(root, text='enter the msg to display: ')
+entry_msg = Entry(root)
+
+
+lbl_name.grid(row=0, column=0)
+entry_name.grid(row=0, column=1)
+
+lbl_year.grid(row=1, column=0)
+entry_year.grid(row=1, column=1)
+
+lbl_month.grid(row=2, column=0)
+entry_month.grid(row=2, column=1)
+
+lbl_day.grid(row=3, column=0)
+entry_day.grid(row=3, column=1)
+
+lbl_hour.grid(row=4, column=0)
+entry_hour.grid(row=4, column=1)
+
+lbl_min.grid(row=5, column=0)
+entry_min.grid(row=5, column=1)
+
+lbl_msg.grid(row=6, column=0)
+entry_msg.grid(row=6, column=1)
+
+
+def btn_click():
+    year = int(entry_year.get())
+    month = int(entry_month.get())
+    day = int(entry_day.get())
+    hour = int(entry_hour.get())
+    minute = int(entry_min.get())
+    msg = entry_msg.get()
+    name = entry_name.get()
+
+    client_date = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+
+    countdown(client_date, msg, name)
+
+
+
+btn = Button(root, text='submit', command=btn_click)
+btn.grid(row=7, column=1)
+
+root.mainloop()
+
+# todo
+# better gui
+# control variables
